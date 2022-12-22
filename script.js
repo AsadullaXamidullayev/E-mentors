3e34f2c9858daf5a47146a2acf91b67b533a17f2
AsadullaXamidullayev <xamidullayev.web.dev@gmail.com>
2022-12-22 18:56:21 +0300

    ozgartirdim

---
+++ script.js
@@ -0,0 +1,121 @@
+
+
+// let login = prompt("login parolini kirit")
+// let login1 = prompt("login parrol notogri")
+// let admin1 = prompt("admin parrol kirit")
+// let admin2 = prompt("admin parrol no togri")
+// let son = (12345)
+
+// if(login > son && login < son){
+//     let login = prompt('login paroli notogri')
+
+//     if(login = son){
+//         alert('siz royhatdan otdingiz')
+//     }
+// }
+
+// let son = prompt("login kirit")
+
+
+// if(son < 12345){
+//     alert(son + ' xato 404')
+//     let son2 = prompt("login krit")
+//     if(son2 > 12345){
+//         alert(son2 + 'xato 404')
+//         let son3 = prompt("login kirit")
+//         if(son3 = 12345){
+//             let adminID = prompt("Admin login kirit")
+//             let adminLogin = "admin"
+//             if(adminID < adminLogin){
+//                 alert(adminID + ' Admin id xato 404')
+//                 let adminID1 = prompt("Admin login kirit")
+//                 let adminLogin2 = "admin"
+//                 if (adminID1 > adminLogin2)
+//                 alert('xato admin login')
+//                 let adminID2 = prompt("admin login kiriting")
+//                 let adminLogin3 = ("admin")
+//                 if(adminID2 = adminLogin3){
+//                     alert('salom')
+//                 }
+
+//             }
+//         }
+//     }
+// }
+
+
+
+// let isim = prompt("Salom ismingizni kiriting");
+// let son1 = +prompt("Hohlagan qiymat kiriting");
+// let son2 = +prompt("Hohlagan qiymat kiriting");
+
+// if(son1 > son2 && son2 > son1){
+//     alert(son1  + ' > ' +  son2)
+// } else if(son1<son2 && son2 < son1){
+//     alert(son1 + ' < ' + son2 )
+// }
+
+
+// let test = prompt("Ikkinchi jahon urushi qachon boshlangan");
+
+// if(test > 1941){
+//     alert('bu vaxtda urish boshlanmagan edi')
+// }
+// else if(test < 1941){
+//     alert("bu voxtda urush bovotgan edi")
+// }
+// else{
+//     alert('Togri javob topdingiz')
+// }
+
+
+
+
+// let isim = prompt("Salom Ismingizn kiriting")
+// let son = +prompt("Brinchi sonni kiriting")
+// let son1 = +prompt("Ikkinchi sonni kiriting")
+// let son2 = +prompt("Uchinchi sonni kiriting")
+
+// if (son > son1 && son1 > son2){
+//     alert(isim + ' '+ son + ' eng katta son')
+// }
+// else if(son > son2 && son2 > son1){
+//     alert(isim + ' ' + son + ' Eng katta son')
+// }
+// else if(son1 > son2 && son2 > son){
+//     alert(isim + ' ' + son1 + ' Eng katta son')
+// }
+// else if(son1 > son && son > son2){
+//     alert(isim + ' ' + son1 + ' Eng katta son')
+// }
+// else if(son1 > son2 && son2 > son ){
+//     alert(isim + ' ' + son2 && ' Eng katta son')
+// }
+// else if(son2 > son1 && son1 > son){
+//     alert(isim + ' ' + son2 + ' Eng katta son')
+// }
+// else if(son2 > son && son > son1){
+//     alert(isim + ' ' + son2 + ' Eng katta son')
+// }
+// else{
+//     alert(isim + ' hammasi teng ' + son1 + ' = ' + son2)
+// }
+// orta arifmetik
+
+// let son = +prompt("Hohlaga son yosh")
+// let son1 = +prompt("ikkinchi hohlagan son yoz")
+// let son2 = +prompt("Uchinchi hohlagan son yoz")
+// let javob = son + son1 + son2
+// let umumiy = javob / 3
+
+// alert(umumiy + ' orta arifmetik ga tegishli')
+
+
+
+let son = +prompt(" Hosoblamoqchi bolgan soningizni kiriting ")
+let son1 = +prompt("qaysi amalda foydalanasiz")
+let son2 = +prompt("Hosoblamoqchi bolgan soningizni kiriting")
+let son3 = '+'
+
+
+
